668c688b052f43d86332562ba89d3db21b423429
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-07-05 03:04:18 +0700

    initial commit, gateposing rn
    using phaser 4  template (vite bundller)

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Brief Gatepose Entrance Test
+
+are you a gatekeeper? are you a poser? or maybe, are you both (read: a gateposer)?
---
+++ src/game/main.ts
@@ -0,0 +1,25 @@
+import { Boot } from './scenes/Boot';
+import { GameOver } from './scenes/GameOver';
+import { Game as MainGame } from './scenes/Game';
+import { MainMenu } from './scenes/MainMenu';
+import { AUTO, Game, Scale } from 'phaser';
+import { Preloader } from './scenes/Preloader';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: AUTO,
+  width: 1024,
+  height: 768,
+  parent: 'game-container',
+  backgroundColor: '#028af8',
+  scale: {
+    mode: Scale.FIT,
+    autoCenter: Scale.CENTER_BOTH,
+  },
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+};
+
+const StartGame = (parent: string) => {
+  return new Game({ ...config, parent });
+};
+
+export default StartGame;
